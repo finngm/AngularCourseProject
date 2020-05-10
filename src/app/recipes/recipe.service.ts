11048a112276dec7efdbe7b2fbaf7fc2cb05a354
FinnGM <finngm@virginmedia.com>
2020-05-10 14:55:17 +0100

    282: transforming response data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,26 +8,27 @@ import { Subject } from 'rxjs';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Apple Pie', 
-            'A pie made of apples', 
-            'https://cdn.pixabay.com/photo/2018/08/09/13/35/apple-pie-3594535_960_720.jpg', 
-            [ new Ingredient('Apple', 3), new Ingredient('Pastry', 1) ]
-        ),
-        new Recipe(
-            'Mango Pie', 
-            'A pie made of mangos', 
-            'https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/mango_pie_18053_16x9.jpg', 
-            [ new Ingredient('Mango', 4), new Ingredient('Pastry', 1) ]
-        ),
-        new Recipe(
-            'Regular Pie',
-            'A pieee?!',
-            'https://upload.wikimedia.org/wikipedia/commons/4/4b/Apple_pie.jpg',
-            [ new Ingredient('Pie', 1), new Ingredient('Pastry', 100) ]
-        ), 
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Apple Pie', 
+    //         'A pie made of apples', 
+    //         'https://cdn.pixabay.com/photo/2018/08/09/13/35/apple-pie-3594535_960_720.jpg', 
+    //         [ new Ingredient('Apple', 3), new Ingredient('Pastry', 1) ]
+    //     ),
+    //     new Recipe(
+    //         'Mango Pie', 
+    //         'A pie made of mangos', 
+    //         'https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/mango_pie_18053_16x9.jpg', 
+    //         [ new Ingredient('Mango', 4), new Ingredient('Pastry', 1) ]
+    //     ),
+    //     new Recipe(
+    //         'Regular Pie',
+    //         'A pieee?!',
+    //         'https://upload.wikimedia.org/wikipedia/commons/4/4b/Apple_pie.jpg',
+    //         [ new Ingredient('Pie', 1), new Ingredient('Pastry', 100) ]
+    //     ), 
+    // ];
+    private recipes: Recipe[] = [];
 
     constructor(private shoppingListService: ShoppingListService) {}
 
