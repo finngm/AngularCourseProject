61b6ea60aeeefaa51e64cb9d20c6326e6e3d00dd
FinnGM <finngm@virginmedia.com>
2020-05-11 23:12:26 +0100

    294: loading spinner and error handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -4,10 +4,13 @@ import { AuthService } from './auth.service';
 
 @Component({
     selector: 'app-auth',
-    templateUrl: './auth.component.html'
+    templateUrl: './auth.component.html',
+    styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
     isLoginMode = true;
+    isLoading = false;
+    error: string = null;
 
     constructor(private authService: AuthService) {}
 
@@ -22,15 +25,20 @@ export class AuthComponent {
         const email = form.value.email;
         const password = form.value.password;
 
+        this.isLoading = true;
+
         if (this.isLoginMode) {
 
         } else {
             this.authService.signup(email, password).subscribe(
                 resData => {
                     console.log(resData);
+                    this.isLoading = false;
                 },
                 error => {
                     console.log(error);
+                    this.error = 'An error occurred';
+                    this.isLoading = false;
                 }
             );
         }
