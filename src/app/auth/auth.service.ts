6de0b0234204c6741f7595f02af7f12aac51086a
FinnGM <finngm@virginmedia.com>
2020-05-13 18:36:53 +0100

    304: auto logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -18,6 +18,7 @@ export interface AuthResponseData {
 @Injectable({providedIn: 'root'})
 export class AuthService {
     user = new BehaviorSubject<User>(null);
+    private expirationTimer: any;
 
     constructor(private http: HttpClient, private router: Router) {}
 
@@ -74,18 +75,31 @@ export class AuthService {
 
         if (loadedUser.token) {
             this.user.next(loadedUser);
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+            this.autoLogout(expirationDuration);
         }
     }
 
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if (this.expirationTimer) {
+            clearTimeout(this.expirationTimer);
+        }
+    }
+
+    autoLogout(expirationDuration: number) {
+        this.expirationTimer = setTimeout(() => {
+            this.logout();
+        }, expirationDuration);
     }
 
     private handleAuthentication(email: string, userId: string,  token: string, expiresIn: number) {
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
         this.user.next(user);
+        this.autoLogout(expiresIn * 1000);
         localStorage.setItem('userData', JSON.stringify(user));
     }
 
