15e1ea7df288ddd82439b68b202ea04834ea32f2
FinnGM <finngm@virginmedia.com>
2020-05-13 18:17:44 +0100

    303: auto-login

--- src/app/auth/user.model.ts
+++ src/app/auth/user.model.ts
@@ -6,6 +6,7 @@ export class User {
         private _tokenExpirationDate: Date
     ) {}
 
+    // returns null if token has exipred, return the token otherwise
     get token() {
         if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
             return null;
