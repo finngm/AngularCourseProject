46042cc667eb96e938805e28e5a3b0b67fb613b5
FinnGM <finngm@virginmedia.com>
2020-05-23 22:02:24 +0100

    367: login via effects

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -3,6 +3,9 @@ import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponseData } from './auth.service';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.action';
 
 @Component({
     selector: 'app-auth',
@@ -14,7 +17,7 @@ export class AuthComponent {
     isLoading = false;
     error: string = null;
 
-    constructor(private authService: AuthService, private router: Router) {}
+    constructor(private authService: AuthService, private router: Router, private store: Store<fromApp.AppState>) {}
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -30,7 +33,8 @@ export class AuthComponent {
 
         this.isLoading = true;
         if (this.isLoginMode) {
-            authObs = this.authService.login(email, password)
+            this.store.dispatch(new AuthActions.LoginStart({email: email, password: password}))
+            // authObs = this.authService.login(email, password)
         } else {
             authObs = this.authService.signup(email, password)
         }
