91997c6da051eefa860a38eb6d71a51f8c41d05a
FinnGM <finngm@virginmedia.com>
2020-05-25 20:39:40 +0100

    380: fetching recipes & using resolver

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from '../auth/store/auth.action';
-
+import * as RecipeActions from '../recipes/store/recipe.action';
 
 @Component({
   selector: 'app-header',
@@ -36,7 +36,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   onFetchData() {
-    this.dataStorageService.fetchRecipes().subscribe();
+    // this.dataStorageService.fetchRecipes().subscribe();
+    this.store.dispatch(new RecipeActions.FetchRecipes());
   }
 
   onLogout() {
